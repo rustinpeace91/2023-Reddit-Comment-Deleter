b67d5f75063be3b9828c5f930626b358923dea0e
Isaac Lubanko <isaaclubanko@comcast.net>
2023-03-04 02:58:09 +0000

    2 spaces instead of 4

--- reddit-comment-deleter.js
+++ reddit-comment-deleter.js
@@ -1,5 +1,23 @@
 
+/*
 
+2023 Reddit Comment Deleter. 
+Based partially on this: https://gist.github.com/jziggas/db8f9ffd296b49d484725a52c3f92406
+
+Script that will work in a pinch to delete reddit comments. Deletes one comment every 2 seconds. Run it in the background
+and go do something else to scrub your reddit history.  Uses a pretty brute force approach of just finding all the overflow icons, clicking on one at a time, then looking
+for elements in the page with the word 'delete' in them and clicking on them. Not very precise or smart, but worked for me.  Since reddit switches up their layout every few years
+Do not count on this to work very long, but if they still require the process of clicking on an overflow bar, selecting delete, then clicking an 'are you sure?' modal, then the basic
+process should be the same even if you have to switch up the css selectors. 
+
+INSTRUCTIONS: 
+
+While logged in, Go to https://www.reddit.com/user/<yourusernamehere>/comments
+
+and paste this in the console
+
+
+*/ 
 
 // if the script cannot find the elements
 // right click click on the '...', select 'Inspect Element' and paste what you see under 'class' in the ICON_NAME variable
@@ -37,4 +55,5 @@ let interval = setInterval(() => {
 	var burger = allBurgers[SAVE_FIRST_COMMENTS]
 	deleteComment(burger)
 }, 2000)
+
 
